253bca69bcbde9016d0ee832c4d823c98488a9d8
hakanssonanton <hakanssonanton@live.com>
2026-02-05 13:13:41 +0100

    namnbyte och githubuppladdning

--- github_setup.R
+++ github_setup.R
@@ -8,5 +8,9 @@ library(haven)
 
 
 # edit and track changes --------------------------------------------------
+library("usethis")
+#install.packages("gitcreds")
+library(gitcreds)
 
-
+create_github_token()
+gitcreds::gitcreds_set()
